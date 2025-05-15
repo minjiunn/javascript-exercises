2c469c5dad29dc4329904ff2e7b2774cb644710e
minjiunn <cminjiun@gmail.com>
2025-05-15 17:18:11 +0800

    Completed exercise 09: sumAll

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,30 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    if(!(typeof start === 'number') || !(typeof end === 'number')) {
+        return "ERROR";
+    } // this statement is not needed bc the Integer check alr handles it
+    if(!Number.isInteger(start) || !Number.isInteger(end)) {
+        return "ERROR";
+    } 
+    if (start < 0 || end < 0) {
+        return "ERROR";
+    }
 
+    let min;
+    let max;
+
+    if (start <= end) {
+        min = start;
+        max = end;
+    } else {
+        min = end;
+        max = start;
+    }
+
+    sum = 0;
+    for (let i = min; i <= max; i++) {
+        sum = sum + i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
