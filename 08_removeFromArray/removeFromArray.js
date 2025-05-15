41d1f4784476d5ad6b61cebcc8f7312e566cc00c
minjiunn <cminjiun@gmail.com>
2025-05-15 16:57:10 +0800

    Completed exercise 8: removeFromArray

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...vals) {
+    let new_arr = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (!vals.includes(arr[i])) {
+            new_arr.push(arr[i])
+        }
+    }
+    return new_arr
 };
 
 // Do not edit below this line
